4278c14f80720e1c75a3edbc08f60724d0e5090f
Sheldon <Sheldon.Wei.Xiaodong@icloud.com>
2025-06-03 17:46:18 +0800

    forked a new repo and built done locally

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ const terminalElement = document.getElementById("terminal") as HTMLElement;
 
 const fitAddon = new FitAddon();
 const term = new Terminal({
-  fontFamily: "Jetbrains Mono",
+  fontFamily: "Jetbrains Mono, MesloLGS NF",
   theme: {
     background: "rgb(47, 47, 47)",
   },
